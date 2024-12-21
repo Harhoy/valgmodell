c7df5070b2ce8377ed4911b128b3504ca2426859
haraldhoyem <haraldhoyem@gmail.com>
2024-12-21 09:25:08 +0100

    legger til innlesing og vekting

--- modell/vektingsmodell.py
+++ modell/vektingsmodell.py
@@ -1,10 +1,8 @@
-
-
 import sqlite3
 from sqlalchemy import text
 from copy import deepcopy
-
-
+import numpy as np
+import datetime
 '''
     conn = sqlite3.connect("db/Valg_db.db")
     cursor = conn.cursor()
@@ -17,7 +15,7 @@ from copy import deepcopy
 
 class Vektingsmodell:
 
-    def __init__(self, database, dateA, dateB):
+    def __init__(self, database, dateA = 15, dateB = 30):
 
         #Database with downloaded data on polls
         self._database = database
@@ -25,13 +23,18 @@ class Vektingsmodell:
         self._conn = sqlite3.connect(self._database)
         #Cursor
         self._cursor = self._conn.cursor()
+        #Connecting party name and number
+        self._partiNavnNummer = {'AP':0, 'H':1, 'Frp':2, 'SV':3, 'Sp':4, 'KrF':5, 'V':6, 'MDG':7, 'R':8, 'A':9}
+
+        self._dateA = dateA
+        self._dateB = dateB
 
         #Extract raw data from database
     def getData(self, omraade):
 
         query = "SELECT AP, H, Frp, SV, Sp, KrF, V, MDG, R, A, Utvalgsstorrelse, Dato FROM Malinger WHERE Omraade == " +  "'" + omraade +  "'"
 
-        results = []
+        self._results = []
         for row in self._conn.execute(query):
             temp = {}
             temp['AP'] = row[0]
@@ -46,12 +49,31 @@ class Vektingsmodell:
             temp['A'] = row[9]
             temp['N'] = row[10]
             temp['Dato'] = row[11]
-            results.append(deepcopy(temp))
+            self._results.append(deepcopy(temp))
 
-        return results
+        #return results
+
+    def numpfy(self):
+
+        #Matrix of the observed party shares
+        self._shareMarix = np.zeros((len(self._results),len(self._partiNavnNummer)))
+        #Array with the number of observations
+        self._observations = np.zeros((len(self._results),1))
+        #Array with dates
+        self._dates = np.zeros((len(self._partiNavnNummer),1))
+
+        #Coupling shares with parties, observations and dates
+        for i in range(len(self._results)):
+            for parti, index in self._partiNavnNummer.items():
+                self._shareMarix[i][index] = self._results[i][parti]
+
+            self._observations[i] = self._results[i]['N']
+
+
 
 
 if __name__ == "__main__":
 
     vm = Vektingsmodell("data/poll/db/Valg_db.db")
     vm.getData("Hele landet")
+    vm.numpfy()
