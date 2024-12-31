9aceae0b767f4abe118c3d06a1baeffc0d3fed04
haraldhoyem <haraldhoyem@gmail.com>
2024-12-31 09:57:48 +0100

    startet på server-opplegget

--- backend/main.py
+++ backend/main.py
@@ -6,21 +6,62 @@ import json
 import sqlite3
 
 
-
 #Selve appen
 app = Flask(__name__)
 
+#Telling the app where the database is located
+app.config['SQLALCHEMY_DATABASE_URI'] = r"sqlite:///" + "/Users/bruker/Documents/Programmering/2024/valgside/valgmodell/modell/data/databaser/mainDB_TEST.db"
 
-conn = sqlite3.connect("/Users/bruker/Documents/Programmering/2024/valgside/valgmodell/modell/data/databaser/mainDB_TEST.db")
-cur = conn.cursor()
-
+#Databse
+db = SQLAlchemy(app) #ny database og sender inn appen her
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/resultater_part_mandater")
+
+@app.route("/getDistricts")
+def get_districts():
+    QUERY = text("SELECT ID Name FROM Districts")
+    RETURN_VAL = {}
+    result = db.engine.execute(QUERY)
+    for row in result:
+        RETURN_VAL[row[0]] = row[1]
+    return json.dumps(RETURN_VAL)
+
+#----------------------------------------------
+#Getting counts for each county for newest
+#----------------------------------------------
+@app.route("/resultater_part_mandater", methods = ['POST'])
 def resultater_part_mandater():
+
+    if request.method == "POST":
+
+        CURRENT_SIM = db.engine.execute("select max(id) from Simulering").fetchone()[0]
+        #DISTRICT = request.args.get("district", default = 1, type = int)
+        DISTRICT = request.json.get("district")
+        RETURN_VAL = {}
+
+        QUERY = text("SELECT Mandater_distrikt, Mandater_utjevning, Mandater_total, Parti FROM Resultater_parti WHERE SimuleringsID == " +  "'" + str(CURRENT_SIM) +  "'" + " AND " + " Fylke == " +  "'" + str(DISTRICT) +  "'"  + " ORDER BY Parti")
+
+        result = db.engine.execute(QUERY)
+
+        for row in result:
+
+            distrikt = row[0]
+            utjevning = row[1]
+            total = row[2]
+            parti = row[3]
+
+            RETURN_VAL[parti] = {'distrikt': distrikt, 'utjevning':utjevning, 'total': total}
+
+        return json.dumps(RETURN_VAL)
+
+#----------------------------------------------
+#Getting count for total for newest
+#----------------------------------------------
+@app.route("/resultater_part_mandater_total")
+def resultater_part_mandater_total():
     pass
 
 
--- backend/static/js/main.js
+++ backend/static/js/main.js
@@ -1,6 +1,66 @@
 
+const partyList = ['Ap', 'Frp','H', 'KrF','MDG','R','SP','SV','V'];
 
-//window.addEventListener("load", () => {
+const partyListLookup = {'Arbeiderpartiet':1, 'Fremskrittspartiet':2,'Høyre':3, 'Kristelig folkeparti':4,'Miljøpartiet de Grønne':5,'Rødt':6,'Senterpartiet':7,'Sosialistisk Venstreparti':8,'Venstre':9};
+
+window.addEventListener("load", () => {
+
+  let resp = fetch(`/resultater_part_mandater`, {
+    method: 'POST',
+    body: JSON.stringify({
+      district: 2
+    }),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8"
+    }
+  })
+  .then((response) => response.json())
+  .then(json => {
+    console.log(json);
+  })
+
+  //Legger til addEventListener paa valgbutton
+  const chooseDistrict = document.getElementById("chooseDistrict");
+  //chooseDistrict.addEventListener("click", function(e){]
+
+  const districtList = document.createElement("div");
+  districtList.classList.add("dropdown-menu");
+  districtList.setAttribute("aria-labelledby","chooseDistrict");
+
+  const dropdownMenu = document.getElementById("dropdownDistrict");
+  dropdownMenu.appendChild(districtList);
+
+  for (const [key, value] of Object.entries(partyListLookup)) {
+    console.log(key);
+  }
+
+/*
+<div class="dropdown-menu" aria-labelledby="chooseDistrict">
+  <a class="dropdown-item" href="#">Action</a>
+  <a class="dropdown-item" href="#">Another action</a>
+  <a class="dropdown-item" href="#">Something else here</a>
+</div>
+
+*/
+
+
+
+});
+
+
+
+//<canvas id="myChart" style="width:100%;max-width:700px"></canvas>
+
+function addPlotSinglePartyCounts(countVector) {
+
+
+}
+
+
+function chooseDistrictClick(){
+
+}
+
 
 const xValues = [50,60,70,80,90,100,110,120,130,140,150];
 const xValues2 = [10,10,170,180,190,1100,110,130,140,150,160];
@@ -37,5 +97,3 @@ const myChart = new Chart("myChart", {
         }
     }
 });
-
-//});
